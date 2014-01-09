04d23577a93e56b826fa00124294df8b6567e37e
Matt Ballentine <pctech28031@gmail.com>
2014-01-09 13:17:00 -0500

    update232

--- js/main.js
+++ js/main.js
@@ -1,1 +1,18 @@
+var red = [0, 100, 63];
+var orange = [40, 100, 60];
+var green = [106, 100, 40];
+var blue = [196, 77, 55];
+var purple = [280, 50, 60];
 
+var myName = "Abby Cook";
+var letterColors = [green, blue, purple]
+if(22 > 12) {
+    bubbleShape = "circle";
+}
+else {
+    bubbleShape = "square";
+}
+
+
+drawName(myName, letterColors);
+bounceBubbles();
